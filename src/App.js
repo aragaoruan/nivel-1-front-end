cf8f8d5ca2ec136f247f09d0dd9f7ee62b2d6f8e
Ruan Aragao <aragao.ruan@gmail.com>
2020-05-07 19:36:12 -0300

    feat: final desafio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header'
+import api from './services/api';
 
 function App() {
+  const [projects, setProjects] = useState([]);  
+
+  useEffect(() => {
+    async function getProjects() {
+      const response = await api.get('projects');
+
+      setProjects(response.data);
+
+    }
+
+    getProjects();
+  },[]);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title : `Novo Projeto ${Date.now()}`,
+      owner : "Ruan Aragão"
+    });
+
+    setProjects([...projects, response.data])
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="Projetos" />
+
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject} >Add project</button>
+
+    </>
+   
   );
 }
 
